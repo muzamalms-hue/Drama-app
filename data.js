8d744af9e1ccd2728b4016c60633d649e9355f71
muzamalms-hue <muzamalms@gmail.com>
2026-03-25 14:57:25 +0500

    Update data.js

--- data.js
+++ data.js
@@ -2,26 +2,12 @@ const dramas = [
   {
     title: "Drama 1",
     thumbnail: "https://img.youtube.com/vi/VIDEO_ID1/0.jpg",
-    episodes: [
-      {
-        name: "Episode 1",
-        video: "https://www.youtube.com/embed/VIDEO_ID1"
-      },
-      {
-        name: "Episode 2",
-        video: "https://www.youtube.com/embed/VIDEO_ID2"
-      }
-    ]
+    video: "https://www.youtube.com/embed/VIDEO_ID1"
   },
 
   {
     title: "Drama 2",
-    thumbnail: "https://img.youtube.com/vi/VIDEO_ID3/0.jpg",
-    episodes: [
-      {
-        name: "Episode 1",
-        video: "https://www.youtube.com/embed/VIDEO_ID3"
-      }
-    ]
+    thumbnail: "https://img.youtube.com/vi/VIDEO_ID2/0.jpg",
+    video: "https://www.youtube.com/embed/VIDEO_ID2"
   }
 ];

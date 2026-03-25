18e757b4b9048083deb8df6c0d5d8d61ea157d68
muzamalms-hue <muzamalms@gmail.com>
2026-03-25 15:24:21 +0500

    Update drama titles and thumbnails in data.js

--- data.js
+++ data.js
@@ -1,13 +1,47 @@
 const dramas = [
   {
-    title: "Drama 1",
-    thumbnail: "https://img.youtube.com/vi/VIDEO_ID1/0.jpg",
-    video: "https://www.youtube.com/embed/VIDEO_ID1"
+    title: "Crorepati ki Dulhaniya",
+    thumbnail: "https://img.youtube.com/vi/HJYPm6ZAXZY/hqdefault.jpg",
+    video: "https://www.youtube.com/embed/HJYPm6ZAXZY"
   },
-
   {
-    title: "Drama 2",
-    thumbnail: "https://img.youtube.com/vi/VIDEO_ID2/0.jpg",
-    video: "https://www.youtube.com/embed/VIDEO_ID2"
+    title: "CEO ki Secret Biwi",
+    thumbnail: "https://img.youtube.com/vi/LrQUT5_v8eg/hqdefault.jpg",
+    video: "https://www.youtube.com/embed/LrQUT5_v8eg"
+  },
+  {
+    title: "Crorepati Dulhaniya",
+    thumbnail: "https://img.youtube.com/vi/f49So1y5xg0/hqdefault.jpg",
+    video: "https://www.youtube.com/embed/f49So1y5xg0"
+  },
+  {
+    title: "Tu Raja Mai Sikandar",
+    thumbnail: "https://img.youtube.com/vi/O8rmofcUSpc/hqdefault.jpg",
+    video: "https://www.youtube.com/embed/O8rmofcUSpc"
+  },
+  {
+    title: "Crorepati Chaiwala",
+    thumbnail: "https://img.youtube.com/vi/4VpGW4s4hjw/hqdefault.jpg",
+    video: "https://www.youtube.com/embed/4VpGW4s4hjw"
+  },
+  {
+    title: "Crorepati Dulhaniya 2",
+    thumbnail: "https://img.youtube.com/vi/f49So1y5xg0/hqdefault.jpg",
+    video: "https://www.youtube.com/embed/f49So1y5xg0"
+  },
+  {
+    title: "Crorepati ki Dulhaniya 2",
+    thumbnail: "https://img.youtube.com/vi/HJYPm6ZAXZY/hqdefault.jpg",
+    video: "https://www.youtube.com/embed/HJYPm6ZAXZY"
+  },
+  {
+    title: "CEO ki Secret Biwi 2",
+    thumbnail: "https://img.youtube.com/vi/LrQUT5_v8eg/hqdefault.jpg",
+    video: "https://www.youtube.com/embed/LrQUT5_v8eg"
+  },
+  {
+    title: "Crorepati Dulhaniya 3",
+    thumbnail: "https://img.youtube.com/vi/f49So1y5xg0/hqdefault.jpg",
+    video: "https://www.youtube.com/embed/f49So1y5xg0"
   }
 ];

6d96572fd3f6cf91c615096c7a564dd8b547e8d9
muzamalms-hue <muzamalms@gmail.com>
2026-03-27 14:14:20 +0500

    Change categories to 'popular' for multiple entries

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ const dramas = [
     title: "Crorepati ki Secret Life",
     video: "HJYPm6ZAXZY",
     thumbnail: "https://img.youtube.com/vi/HJYPm6ZAXZY/hqdefault.jpg",
-    category: "trending, popular"
+    category: "popular"
   },
   {
     title: "Crorepati ki Dulhaniya",
@@ -15,18 +15,18 @@ const dramas = [
     title: "Hindi Drama",
     video: "HJYPm6ZAXZY",
     thumbnail: "https://img.youtube.com/vi/HJYPm6ZAXZY/hqdefault.jpg",
-    category: "hindi"
+    category: "popular"
   },
   {
     title: "English Drama",
     video: "LrQUT5_v8eg",
     thumbnail: "https://img.youtube.com/vi/LrQUT5_v8eg/hqdefault.jpg",
-    category: "english"
+    category: "popular"
   },
   {
     title: "Korean Drama",
     video: "O8rmofcUSpc",
     thumbnail: "https://img.youtube.com/vi/O8rmofcUSpc/hqdefault.jpg",
-    category: "korean"
+    category: "popular"
   }
 ];

35798d389d62aa9dc0a908fbac246e89d2c7a7b6
muzamalms-hue <muzamalms@gmail.com>
2026-03-27 12:54:26 +0500

    Remove duplicate entry from data.js

--- data.js
+++ data.js
@@ -5,18 +5,10 @@ const dramas = [
     thumbnail: "https://img.youtube.com/vi/HJYPm6ZAXZY/hqdefault.jpg",
     category: "trending"
   },
-
-  {
-    title: "Crorepati ki Secret Life",
-    video: "HJYPm6ZAXZY",
-    thumbnail: "https://img.youtube.com/vi/HJYPm6ZAXZY/hqdefault.jpg",
-    category: "popular"
-  },
-
   {
     title: "Crorepati ki Dulhaniya",
     video: "f49So1y5xg0",
     thumbnail: "https://img.youtube.com/vi/f49So1y5xg0/hqdefault.jpg",
     category: "popular"
-  }
+  },
 ];

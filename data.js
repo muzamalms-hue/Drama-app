8d6cfbbb8f1e21e827a6dc0e04856c9b3d080f9d
muzamalms-hue <muzamalms@gmail.com>
2026-03-27 12:03:46 +0500

    Update data.js

--- data.js
+++ data.js
@@ -1,47 +1,15 @@
 const dramas = [
   {
+    title: "Crorepati ki Secret Life",
+    video: "HJYPm6ZAXZY",
+    thumbnail: "https://img.youtube.com/vi/HJYPm6ZAXZY/hqdefault.jpg",
+    category: "trending"
+  },
+
+  {
     title: "Crorepati ki Dulhaniya",
+    video: "HJYPm6ZAXZY",
     thumbnail: "https://img.youtube.com/vi/HJYPm6ZAXZY/hqdefault.jpg",
-    video: "https://www.youtube.com/embed/HJYPm6ZAXZY"
-  },
-  {
-    title: "CEO ki Secret Biwi",
-    thumbnail: "https://img.youtube.com/vi/LrQUT5_v8eg/hqdefault.jpg",
-    video: "https://www.youtube.com/embed/LrQUT5_v8eg"
-  },
-  {
-    title: "Crorepati Dulhaniya",
-    thumbnail: "https://img.youtube.com/vi/f49So1y5xg0/hqdefault.jpg",
-    video: "https://www.youtube.com/embed/f49So1y5xg0"
-  },
-  {
-    title: "Tu Raja Mai Sikandar",
-    thumbnail: "https://img.youtube.com/vi/O8rmofcUSpc/hqdefault.jpg",
-    video: "https://www.youtube.com/embed/O8rmofcUSpc"
-  },
-  {
-    title: "Crorepati Chaiwala",
-    thumbnail: "https://img.youtube.com/vi/4VpGW4s4hjw/hqdefault.jpg",
-    video: "https://www.youtube.com/embed/4VpGW4s4hjw"
-  },
-  {
-    title: "Crorepati Dulhaniya 2",
-    thumbnail: "https://img.youtube.com/vi/f49So1y5xg0/hqdefault.jpg",
-    video: "https://www.youtube.com/embed/f49So1y5xg0"
-  },
-  {
-    title: "Crorepati ki Dulhaniya 2",
-    thumbnail: "https://img.youtube.com/vi/HJYPm6ZAXZY/hqdefault.jpg",
-    video: "https://www.youtube.com/embed/HJYPm6ZAXZY"
-  },
-  {
-    title: "CEO ki Secret Biwi 2",
-    thumbnail: "https://img.youtube.com/vi/LrQUT5_v8eg/hqdefault.jpg",
-    video: "https://www.youtube.com/embed/LrQUT5_v8eg"
-  },
-  {
-    title: "Crorepati Dulhaniya 3",
-    thumbnail: "https://img.youtube.com/vi/f49So1y5xg0/hqdefault.jpg",
-    video: "https://www.youtube.com/embed/f49So1y5xg0"
+    category: "popular"
   }
 ];

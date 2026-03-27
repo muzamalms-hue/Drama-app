53252071318c0e6bceff3edf1fd785b240ef2399
muzamalms-hue <muzamalms@gmail.com>
2026-03-27 12:14:23 +0500

    Remove multiple dramas and update thumbnail
    Removed several drama entries from the dramas array and updated the thumbnail for 'Crorepati ki Secret Life'.

--- data.js
+++ data.js
@@ -1,59 +1,14 @@
 const dramas = [
-
   {
     title: "Crorepati ki Secret Life",
     video: "HJYPm6ZAXZY",
-    thumbnail: "https://img.youtube.com/vi/HJYPm6ZAXZY/maxresdefault.jpg",
+    thumbnail: "https://img.youtube.com/vi/HJYPm6ZAXZY/hqdefault.jpg",
     category: "trending"
   },
-
-  {
-    title: "CEO ki Secret Biwi",
-    video: "LrQUT5_v8eg",
-    thumbnail: "https://img.youtube.com/vi/LrQUT5_v8eg/hqdefault.jpg",
-    category: "trending"
-  },
-
-  {
-    title: "Crorepati Dulhaniya",
-    video: "f49So1y5xg0",
-    thumbnail: "https://img.youtube.com/vi/f49So1y5xg0/hqdefault.jpg",
-    category: "trending"
-  },
-
-  {
-    title: "Tu Raja Mai Sikandar",
-    video: "O8rmofcUSpc",
-    thumbnail: "https://img.youtube.com/vi/O8rmofcUSpc/hqdefault.jpg",
-    category: "trending"
-  },
-
-  {
-    title: "Crorepati Chaiwala",
-    video: "4VpGW4s4hjw",
-    thumbnail: "https://img.youtube.com/vi/4VpGW4s4hjw/hqdefault.jpg",
-    category: "popular"
-  },
-
-  {
-    title: "Crorepati Dulhaniya 2",
-    video: "f49So1y5xg0",
-    thumbnail: "https://img.youtube.com/vi/f49So1y5xg0/hqdefault.jpg",
-    category: "popular"
-  },
-
-  {
-    title: "CEO ki Secret Biwi 2",
-    video: "LrQUT5_v8eg",
-    thumbnail: "https://img.youtube.com/vi/LrQUT5_v8eg/hqdefault.jpg",
-    category: "popular"
-  },
-
   {
     title: "Crorepati ki Dulhaniya",
     video: "HJYPm6ZAXZY",
     thumbnail: "https://img.youtube.com/vi/HJYPm6ZAXZY/hqdefault.jpg",
     category: "popular"
   }
-
 ];

96965abfad60096794cee3702923a5465a2ee3e6
muzamalms-hue <muzamalms@gmail.com>
2026-03-28 13:45:26 +0500

    Update data.js

--- data.js
+++ data.js
@@ -1,27 +1,9 @@
 const dramas = [
-
-  // 🔴 HINDI
-  {
-    title: "The Hidden King",
-    video: "https://drive.google.com/uc?export=download&id=1ZhAkhPU30Yo4v038BOdrWbKZC6af7GUa",
-    thumbnail: "https://drive.google.com/uc?export=view&id=1FYhRUsN8stYpa3rNgqcYOq2terWAcdfQ",
-    category: "hindi"
-  },
-
-  // 🔵 POPULAR
   {
     title: "The Hidden King",
     video: "https://drive.google.com/uc?export=download&id=1ZhAkhPU30Yo4v038BOdrWbKZC6af7GUa",
     thumbnail: "https://drive.google.com/uc?export=view&id=1FYhRUsN8stYpa3rNgqcYOq2terWAcdfQ",
     category: "popular"
-  },
-
-  // 🔥 TRENDING
-  {
-    title: "The Hidden King",
-    video: "https://drive.google.com/uc?export=download&id=1ZhAkhPU30Yo4v038BOdrWbKZC6af7GUa",
-    thumbnail: "https://drive.google.com/uc?export=view&id=1FYhRUsN8stYpa3rNgqcYOq2terWAcdfQ",
-    category: "trending"
   }
 
 ];

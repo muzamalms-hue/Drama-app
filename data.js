0c90ba9fab36183e39628d84662407669240888a
muzamalms-hue <muzamalms@gmail.com>
2026-03-28 13:47:44 +0500

    Update data.js

--- data.js
+++ data.js
@@ -1,9 +1,8 @@
 const dramas = [
   {
-    title: "The Hidden King",
+    title: "Test Video",
     video: "https://drive.google.com/uc?export=download&id=1ZhAkhPU30Yo4v038BOdrWbKZC6af7GUa",
     thumbnail: "https://drive.google.com/uc?export=view&id=1FYhRUsN8stYpa3rNgqcYOq2terWAcdfQ",
     category: "popular"
   }
-
 ];

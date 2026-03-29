af9a8dbdbe20c6b99ed76ed7a6cc76be413eb2ae
muzamalms-hue <muzamalms@gmail.com>
2026-03-29 22:55:04 +0500

    Add new drama entry for Archive Video

--- data.js
+++ data.js
@@ -1,5 +1,31 @@
 const dramas = [
   {
+    title: "Crorepati ki Secret Life",
+    video: "HJYPm6ZAXZY",
+    thumbnail: "https://img.youtube.com/vi/HJYPm6ZAXZY/hqdefault.jpg",
+    category: "popular"
+  },
+  {
+    title: "Crorepati ki Dulhaniya",
+    video: "f49So1y5xg0",
+    thumbnail: "https://img.youtube.com/vi/f49So1y5xg0/hqdefault.jpg",
+    category: "popular"
+  },
+  {
+    title: "Secret Crorepati",
+    video: "WM3IIetxf4I",
+    thumbnail: "https://img.youtube.com/vi/WM3IIetxf4I/hqdefault.jpg",
+    category: "popular"
+  },
+  {
+    title: "The Hidden King",
+    video: "9rg53ix6hHk",
+    thumbnail: "https://img.youtube.com/vi/9rg53ix6hHk/hqdefault.jpg",
+    category: "popular"
+  },
+
+  // 🔥 INTERNET ARCHIVE VIDEO
+  {
     title: "Archive Video",
     video: "https://archive.org/embed/lv_0_20260324003702",
     thumbnail: "https://dummyimage.com/300x170/000/fff&text=Archive",
